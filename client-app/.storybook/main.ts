b1d02033916eccd131438a95df9c82f779f4202b
ozrendemonja <11740633+ozrendemonja@users.noreply.github.com>
2024-12-11 10:25:55 +0000

    Add storybook and screenshot tests
    * Add storybook and first test
    
    * Add storybook tests
    
    * Add Playwright screenshot tests
    
    * Update

---
+++ client-app/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(ts|tsx)"],
+  addons: [
+    "@storybook/addon-webpack5-compiler-babel",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    'storybook-addon-mock',
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {
+      fsCache: true,
+      lazyCompilation: true
+    },
+  },
+};
+export default config;

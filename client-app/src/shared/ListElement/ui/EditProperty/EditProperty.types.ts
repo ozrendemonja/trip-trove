8eac43d1a41146c5869f710cf74da3ea1f0b9f59
Ozren Demonja <11740633+ozrendemonja@users.noreply.github.com>
2024-12-15 20:51:13 +0000

    Reafactor

--- client-app/src/shared/ListElement/ui/EditProperty/EditProperty.types.ts
+++ client-app/src/shared/ListElement/ui/EditProperty/EditProperty.types.ts
@@ -1,10 +1,6 @@
 export interface EditPropertyProps {
-    /**
-  * Text to display as the header
-  */
-    text?: string;
-    /**
-     * Flag indicating if the property can be deleted
-     */
-    isOptional?: boolean
+  /**
+* Text to display as the header
+*/
+  text: string;
 }

bda2d2b8d2e01b1a014e7767b7ec1857c315521f
ozrendemonja <11740633+ozrendemonja@users.noreply.github.com>
2024-12-17 20:41:56 +0000

    Enforce project standards with SonarQube, ESLint, and Prettier
    * Enforcing project standards with SonarQube, ESLint, and Prettier
    
    * Add env variables in Docker

---
+++ client-app/tests/ListContinent.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Show spinner while content is loading', async ({ page }) => {
+    await page.goto('http://localhost:6006/iframe.html?id=features-continent-pages-listcontinent--primary');
+
+    await page.waitForSelector('.ms-Spinner-label');
+
+    await expect(page).toHaveScreenshot();
+});
+

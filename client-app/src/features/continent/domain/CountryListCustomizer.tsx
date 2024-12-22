126af58679fdcb53febbcccecb9ceb8c705de1b1
Ozren Demonja <11740633+ozrendemonja@users.noreply.github.com>
2024-12-22 14:06:14 +0000

    Add FE code

---
+++ client-app/src/features/continent/domain/CountryListCustomizer.tsx
@@ -0,0 +1,45 @@
+import { buildColumns, IColumn } from "@fluentui/react";
+import { ListElementCustomizer } from "../../../shared/list-element/ListElement.types";
+import { Country } from "./Country.types.";
+
+export class CountryListCustomizer extends ListElementCustomizer<Country> {
+  constructor(
+    items: Country[],
+    callback: (items: Country[]) => void,
+    callback2: (columns: IColumn[]) => void
+  ) {
+    super(items, callback, callback2);
+    this.callback(items);
+  }
+
+  private setSetupForSortIcon = (column: IColumn): IColumn => {
+    const result = { ...column };
+    if (result.name === "name") {
+      result.showSortIconWhenUnsorted = true;
+      result.isCollapsible = true;
+      result.isMultiline = true;
+      result.minWidth = 100;
+    }
+    return result;
+  };
+
+  private setDefaultLayout = (column: IColumn): IColumn => {
+    const result = { ...column };
+
+    result.ariaLabel = `Operations for ${column.name}`;
+    result.isMultiline = false;
+    result.minWidth = 100;
+    result.isResizable = true;
+
+    return result;
+  };
+
+  public createColumns = (): void => {
+    const columns = buildColumns(this.items, true, this.onColumnClick)
+      .map((column) => this.setSetupForSortIcon(column))
+      .map((column) => this.setDefaultLayout(column));
+
+    this.columns = columns;
+    this.callback2(this.columns);
+  };
+}

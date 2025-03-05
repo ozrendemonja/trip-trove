11e3a9b2fdc7a26104745ca8223f8c2e9b397f50
Ozren Demonja <11740633+ozrendemonja@users.noreply.github.com>
2025-03-05 12:20:54 +0000

    Update

--- backend/src/main/java/com/triptrove/manager/domain/model/Country.java
+++ backend/src/main/java/com/triptrove/manager/domain/model/Country.java
@@ -3,6 +3,9 @@ package com.triptrove.manager.domain.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,14 +17,17 @@ import java.util.Optional;
 @Table(name = "country", uniqueConstraints =
         {@UniqueConstraint(name = "UniqueNameAndContinent", columnNames = {"name", "continent"})}
 )
+@EntityListeners(AuditingEntityListener.class)
 public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @CreatedDate
     @Column(name = "created_on", updatable = false, nullable = false)
     private LocalDateTime createdOn;
 
+    @LastModifiedDate
     @Column(name = "updated_on")
     private LocalDateTime updatedOn;
 
@@ -36,16 +42,6 @@ public class Country {
             mappedBy = "country",
             cascade = CascadeType.PERSIST)
     private List<Attraction> attractions;
-
-    @PrePersist
-    protected void onCreate() {
-        createdOn = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        updatedOn = LocalDateTime.now();
-    }
 
     public Optional<LocalDateTime> getUpdatedOn() {
         return Optional.ofNullable(updatedOn);

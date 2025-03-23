719c71888ec4baa49f089ea715df84dda4e18262
Ozren Demonja <11740633+ozrendemonja@users.noreply.github.com>
2025-03-23 20:19:39 +0000

    Fix failing tests

--- backend/src/test/java/com/triptrove/manager/EmptyTableTest.java
+++ backend/src/test/java/com/triptrove/manager/EmptyTableTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @Transactional
 @AutoConfigureMockMvc
+@Sql(value = "/db/clean-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 class EmptyTableTest extends AbstractIntegrationTest {
     private final static ObjectMapper mapper = new ObjectMapper();
 

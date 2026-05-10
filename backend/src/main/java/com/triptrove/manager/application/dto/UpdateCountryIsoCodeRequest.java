d7b768e3db799f1c6d75c31f49553ebece248b0e
Ozren Demonja <11740633+ozrendemonja@users.noreply.github.com>
2026-05-10 23:36:41 +0100

    Update

--- backend/src/main/java/com/triptrove/manager/application/dto/UpdateCountryIsoCodeRequest.java
+++ backend/src/main/java/com/triptrove/manager/application/dto/UpdateCountryIsoCodeRequest.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.Pattern;
 
 public record UpdateCountryIsoCodeRequest(
         @NotBlank(message = "ISO code may not be null or empty")
-        @Pattern(regexp = "^[A-Za-z]{2}$", message = "ISO code must be a 2-letter ISO 3166-1 alpha-2 code")
+        @Pattern(regexp = "^$|^[A-Za-z]{2}$", message = "ISO code must be a 2-letter ISO 3166-1 alpha-2 code")
         String isoCode) {
 }

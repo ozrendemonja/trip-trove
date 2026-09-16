1207e8fe43cf846872ba693dcfeec4fbbf670dd9
Ozren Demonja <11740633+ozrendemonja@users.noreply.github.com>
2026-09-16 17:17:46 +0100

    Fix inconsistent sidebar sizing and spacing across pages

--- client-app/src/shared/list-element/ListPage.styles.ts
+++ client-app/src/shared/list-element/ListPage.styles.ts
@@ -1,53 +1,10 @@
-import { makeStyles, shorthands, tokens } from "@fluentui/react-components";
+import { makeStyles, tokens } from "@fluentui/react-components";
 
 export const useListPageClasses = makeStyles({
   pageLayout: {
     display: "flex",
     alignItems: "flex-start",
     minHeight: "calc(100vh - 16px)",
-    "& > :first-child": {
-      width: "128px",
-      maxWidth: "128px",
-      height: "auto",
-      boxSizing: "border-box",
-      marginRight: "30px",
-      flexShrink: 0,
-      ...shorthands.padding(
-        tokens.spacingVerticalS,
-        tokens.spacingHorizontalSNudge
-      )
-    },
-    "& > :first-child > :last-child": {
-      marginTop: "36px",
-      fontSize: "11px"
-    },
-    "& > :first-child [aria-label='Navigation menu']": {
-      width: "100%",
-      marginTop: "18px"
-    },
-    "& > :first-child [aria-label='Navigation menu'] > div": {
-      marginBottom: tokens.spacingVerticalXXL
-    },
-    "& > :first-child [aria-label='Navigation menu'] > div:last-child": {
-      marginBottom: 0
-    },
-    "& > :first-child [aria-label='Navigation menu'] a, & > :first-child [aria-label='Navigation menu'] button":
-      {
-        minHeight: "32px",
-        columnGap: "7px",
-        fontSize: tokens.fontSizeBase200,
-        ...shorthands.padding(0, tokens.spacingHorizontalSNudge)
-      },
-    "& > :first-child [aria-label='Navigation menu'] > div > div": {
-      rowGap: 0
-    },
-    "& > :first-child [aria-label='Navigation menu'] svg": {
-      width: "14px",
-      height: "14px"
-    },
-    "& > :first-child .navigationHeaders": {
-      fontSize: "11px"
-    },
     "@media (max-width: 700px)": {
       flexDirection: "column",
       minHeight: 0,
